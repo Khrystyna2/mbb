c7496f1437aa2a1ef4fc7acf2239d27f403a9e2d
Khrystyna2 <L.xrustja@mail.com>
2019-04-17 12:39:10 +0300

    added new image and content on page

--- js/main.js
+++ js/main.js
@@ -6,13 +6,22 @@ $(document).ready(function () {
     $('.menu').slideToggle(600).toggleClass('open');
     $('.nav').addClass('bg-green');
     $('body').toggleClass('overflow');
-    if($('.nav').hasClass('bg-green') && $('.menu').hasClass('open')) {
+    if ($('.nav').hasClass('bg-green') && $('.menu').hasClass('open')) {
       $(".nav").addClass('bg-green');
     } else {
-      setTimeout(function(){
+      setTimeout(function () {
         $(".nav").removeClass('bg-green');
-     }, 500);
+      }, 500);
     }
   });
+
+  // hover
+
+  if ($(window).width() >= 1200) {
+    $('.slider .slider__item').hover(function () {
+      $(this).addClass('big-img');
+      $(this).closest('a').siblings('a').removeClass('big-img');
+    });
+  }
 
 });
