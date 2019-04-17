9e09ce822653de39a3862dd329f0461e9851e294
mac <mac@maxs-MacBook-Pro.local>
2019-04-17 16:39:06 +0300

    4 pages

--- js/main.js
+++ js/main.js
@@ -15,4 +15,16 @@ $(document).ready(function () {
     }
   });
 
+
+
+
 });
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 48.8878223, lng: 24.1620070};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+};
