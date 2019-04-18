b403d686fd205a8189dea2872540e27c5abaa0a2
Khrystyna2 <L.xrustja@mail.com>
2019-04-18 13:25:22 +0300

    added new paga(about-us)

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
     $(this).toggleClass('menu__btn_active');
     $('.menu').slideToggle(600).toggleClass('open');
     $('.nav').addClass('bg-green');
-    $('body').toggleClass('overflow');
     if ($('.nav').hasClass('bg-green') && $('.menu').hasClass('open')) {
       $(".nav").addClass('bg-green');
     } else {
